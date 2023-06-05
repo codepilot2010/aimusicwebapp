bb5199108e99e65204b5b2d91ebe256535f53d98
codepilot2010 <93812156+codepilot2010@users.noreply.github.com>
2023-06-05 12:32:17 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,9 @@
 song="";
 song2="";
+leftwristx=0;
+leftwristy=0;
+rightwristx=0;
+righttwristy=0;
 
 function setup()
 {
@@ -7,6 +11,12 @@ function setup()
     canvas.center();
     video=createCapture(VIDEO);
     video.hide();   
+    posenet=ml5.poseNet(video,modelLoaded);
+    posenet.on('pose',gotPoses); 
+}
+function modelLoaded()
+{
+    console.log('posenet is initialized')
 }
 
 function draw()
@@ -18,4 +28,19 @@ function preload()
 {
     song2=loadSound("bgmusic2.mp3");
     song=loadSound("bgmusic1.mp3");
+}
+
+function gotPoses(results)
+{
+    if(results.length>0)
+    {
+        console.log(results);
+        leftwristx=results[0].pose.leftWrist.x;
+        leftwristy=results[0].pose.leftWrist.y;
+        console.log("left wrist x = "+leftwristx+"left wrist y = "+leftwristy);
+
+        rightwristx=results[0].pose.rightWrist.x;
+        rightwristy=results[0].pose.rightWrist.y;
+        console.log("right wrist x = "+rightwristx+"right wrist y = "+rightwristy);
+    }
 }
